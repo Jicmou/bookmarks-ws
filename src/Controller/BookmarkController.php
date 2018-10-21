ebafdbaa57de251f6610339d645f38c8006e64b3
Jean-Christophe Maigrot <jean-christophe.maigrot@advisorsla.com>
2018-10-21 15:40:52 +0200

    Add Create bookmark endpoint

--- src/Controller/BookmarkController.php
+++ src/Controller/BookmarkController.php
@@ -5,18 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
 
 use App\Entity\Bookmark;
 use App\Service\JSONResponse;
+use App\Service\BookmarkDataRetriever;
 
 class BookmarkController extends AbstractController
 {
 
   private $jsonResponse;
+  private $bookmarkDataRetriever;
+  private $logger;
 
-  public function __construct(JSONResponse $jsonResponse)
-  {
+  private $validUrl = '/^(http(s)?:\/\/(www.)?(vimeo|flickr).com\/)/';
+
+  public function __construct(
+    JSONResponse $jsonResponse,
+    BookmarkDataRetriever $bookmarkDataRetriever,
+    LoggerInterface $logger
+  ) {
     $this->jsonResponse = $jsonResponse;
+    $this->bookmarkDataRetriever = $bookmarkDataRetriever;
+    $this->logger = $logger;
   }
 
   private function getBookmarkListFromORM()
@@ -24,6 +35,11 @@ class BookmarkController extends AbstractController
     return $this->getDoctrine()
       ->getRepository(Bookmark::class)
       ->findAll();
+  }
+
+  private function isUrlValid(string $url)
+  {
+    return preg_match($this->validUrl, $url);
   }
 
   public function getList()
@@ -39,5 +55,27 @@ class BookmarkController extends AbstractController
     } catch (Exception $e) {
       return $this->jsonResponse->getInternalErrorResponse($e);
     }
+  }
+
+  public function create(Request $request)
+  {
+    try {
+      $url = json_decode($request->getContent())->url;
+      if (!$this->isUrlValid($url)) {
+        return $this->jsonResponse->getRequestErrorResponse('invalid url');
+      }
+      $this->logger->info('BookmarkController url: ' . json_encode($url));
+      return $this->jsonResponse->getSuccessResponse(
+        json_encode(
+          array(
+            'bookmark' => $this->bookmarkDataRetriever->getLinkData($url)
+          )
+        )
+      );
+    } catch (Exception $e) {
+      $logger->error('Error in BookmarkController::create: ' . $e->getMessage());
+      return $this->jsonResponse->getInternalErrorResponse($e);
+    }
+
   }
 }
---
+++ src/Service/BookmarkDataRetriever.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+
+class BookmarkDataRetriever
+{
+
+  private $logger;
+
+  private $vimeoApiUrl = 'https://vimeo.com/api/oembed.json';
+
+  private $flickrApiUrl = 'http://www.flickr.com/services/oembed/';
+
+  public function __construct(LoggerInterface $logger)
+  {
+    $this->logger = $logger;
+  }
+
+  public function getLinkData(string $url)
+  {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $this->vimeoApiUrl . '?url=' . $url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTPHEADER => array(
+        "cache-control: no-cache",
+      ),
+    ));
+
+    $response = curl_exec($curl);
+
+    $err = curl_error($curl);
+
+    $info = curl_getinfo($curl);
+
+    $this->logger->info('response: ' . $response);
+    $this->logger->error('error: ' . $err);
+    $this->logger->error('info: ' . json_encode($info));
+    curl_close($curl);
+
+    // if ($err) {
+    //   echo "cURL Error #:" . $err;
+    // } else {
+    //   echo $response;
+    // }
+    return json_decode($response);
+  }
+}
+
+
